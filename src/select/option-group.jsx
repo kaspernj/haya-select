0d15595b06309c19ee46993b5b6de774264d391c
kaspernj <k@spernj.org>
2024-07-12 11:33:54 +0000

    React Native refactor

--- src/select/option-group.jsx
+++ src/select/option-group.jsx
@@ -1,6 +1,7 @@
 import PropTypes from "prop-types"
 import {memo} from "react"
 import {shapeComponent, ShapeComponent} from "set-state-compare/src/shape-component.js"
+import {Text, View} from "react-native"
 
 export default memo(shapeComponent(class OptionGroup extends ShapeComponent {
   static propTypes = {
@@ -9,9 +10,21 @@ export default memo(shapeComponent(class OptionGroup extends ShapeComponent {
 
   render() {
     return (
-      <div className="haya-select--option-group">
-        {this.props.option.text}
-      </div>
+      <View
+        dataSet={{class: "haya-select--option-group"}}
+        style={{
+          paddingTop: 4,
+          paddingRight: 8,
+          paddingBottom: 4,
+          paddingLeft: 8,
+          color: "#000",
+          fontWeight: "bold"
+        }}
+      >
+        <Text>
+          {this.props.option.text}
+        </Text>
+      </View>
     )
   }
 }))
