7dd1c207ff37659e0272c6e91a6a9b7ba272113b
kaspernj <kasper@diestoeckels.de>
2026-01-01 21:08:06 +0000

    Improve system test reliability

--- spec/system/haya-select-render-spec.js
+++ spec/system/haya-select-render-spec.js
@@ -1,12 +1,15 @@
 import "velocious/build/src/testing/test.js";
 import SystemTest from "system-testing/build/system-test.js";
+import timeout from "awaitery/build/timeout.js";
 
 SystemTest.rootPath = "/?systemTest=true";
 
 describe("HayaSelect", () => {
   it("renders in the example app", async () => {
-    await SystemTest.run(async (systemTest) => {
-      await systemTest.findByTestID("hayaSelectRoot");
+    await timeout({timeout: 90000}, async () => {
+      await SystemTest.run({debug: true}, async (systemTest) => {
+        await systemTest.findByTestID("hayaSelectRoot", {timeout: 60000});
+      });
     });
   });
 });
