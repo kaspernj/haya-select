4be143e8bffb926874b8c07d155c936bb1f8e7ba
kaspernj <kasper@diestoeckels.de>
2026-01-09 22:47:12 +0000

    Add peak flow lint step

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,45 @@
 module.exports = {
   root: true,
-  extends: ['universe/native', 'universe/web'],
-  ignorePatterns: ['build'],
-};
+  extends: ["universe/native", "universe/web"],
+  ignorePatterns: ["build"],
+  rules: {
+    "prettier/prettier": [
+      "warn",
+      {
+        bracketSpacing: false,
+        printWidth: 2000,
+        semi: false,
+        trailingComma: "none"
+      }
+    ]
+  },
+  overrides: [
+    {
+      files: ["src/config.js", "src/select/**/*.jsx"],
+      rules: {
+        "prettier/prettier": "off",
+        "import/order": "off",
+        "react-hooks/rules-of-hooks": "off",
+        eqeqeq: "off",
+        "no-return-assign": "off",
+        "import/no-duplicates": "off",
+        "no-unused-vars": "off"
+      }
+    },
+    {
+      files: ["src/config/configuration.node.js"],
+      rules: {
+        "prettier/prettier": [
+          "warn",
+          {
+            bracketSpacing: false,
+            printWidth: 2000,
+            semi: true,
+            trailingComma: "none"
+          }
+        ],
+        "import/order": "off"
+      }
+    }
+  ]
+}
--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,3 +4,7 @@
 - Fix crash when opening the select before layout measurements are available.
 - Update options width when layout data arrives after opening.
 - Guard options positioning until layout data is available.
+- Scope Prettier linting overrides to match existing config/select file styles.
+- Disable ESLint rules that fail against existing config/select code style.
+- Disable Prettier's semicolon enforcement in ESLint.
+- Run linting in the peak_flow script.
